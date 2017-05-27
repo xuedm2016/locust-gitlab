c64584137225ad115cb74c1c5dd0d9a0c8b213e2
xuedongmei <xuedm@chinatelecom.cn>
2017-05-27 14:10:00 +0800

    parameterize the connection parameters to influxdb and mysql

--- locust/connect.py
+++ locust/connect.py
@@ -1,12 +1,21 @@
 # -*- coding:utf-8 -*-
 from influxdb import InfluxDBClient
 import pymysql
+import os
 
 def connect_influx():
-    client = InfluxDBClient('192.168.0.113', 8086, 'root', '', 'statsdb')
+    influxdb_host = os.environ.get('INFLUXDB_HOST', '172.18.143.20')
+    influxdb_user = os.environ.get('INFLUXDB_USER', 'root')
+    influxdb_password = os.environ.get('INFLUXDB_PASSWORD', '')
+    influxdb_dbname = os.environ.get('INFLUXDB_DBNAME','statsdb')
+    client = InfluxDBClient(influxdb_host, 8086, influxdb_user, influxdb_password, influxdb_dbname)
     return client
 
 def connect_mysql():
-    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='mysql', db='statsdb', charset='utf8')
+    mysql_host = os.environ.get('MYSQL_HOST', '172.18.143.49')
+    mysql_user = os.environ.get('MYSQL_USER', 'root')
+    mysql_password = os.environ.get('MYSQL_PASSWORD', 'NewPassw0rd!')
+    mysql_dbname = os.environ.get('MYSQL_DBNAME', 'statsdb')
+    conn = pymysql.connect(host=mysql_host, port=3306, user=mysql_user, passwd=mysql_password, db=mysql_dbname, charset='utf8')
     return conn
 
