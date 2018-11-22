55d15a48223ed437ec77fc8848970cd3cdc8a9ad
xuedm <xuedm@chinatelecom.cn>
2018-11-23 00:26:16 +0800

    debug

--- locust/connect.py
+++ locust/connect.py
@@ -4,19 +4,19 @@ import pymysql
 import os
 
 def connect_influx():
-    influxdb_host = os.environ.get('INFLUXDB_HOST', '172.18.143.20')
+    influxdb_host = os.environ.get('INFLUXDB_HOST', '202.80.193.115')
     influxdb_user = os.environ.get('INFLUXDB_USER', 'root')
-    influxdb_password = os.environ.get('INFLUXDB_PASSWORD', '')
+    influxdb_password = os.environ.get('INFLUXDB_PASSWORD', 'root')
     influxdb_dbname = os.environ.get('INFLUXDB_DBNAME','statsdb')
     client = InfluxDBClient(influxdb_host, 8086, influxdb_user, influxdb_password, influxdb_dbname)
     print ("INFLUXDB HOST is: %s \n" % influxdb_host)
     return client
 
 def connect_mysql():
-    mysql_host = os.environ.get('MYSQL_HOST', '172.18.143.49')
-    mysql_user = os.environ.get('MYSQL_USER', 'root')
+    mysql_host = os.environ.get('MYSQL_HOST', '36.111.164.243')
+    mysql_user = os.environ.get('MYSQL_USER', 'dtpuser')
     mysql_password = os.environ.get('MYSQL_PASSWORD', 'NewPassw0rd!')
-    mysql_dbname = os.environ.get('MYSQL_DBNAME', 'statsdb')
+    mysql_dbname = os.environ.get('MYSQL_DBNAME', 'dtp2')
     conn = pymysql.connect(host=mysql_host, port=3306, user=mysql_user, passwd=mysql_password, db=mysql_dbname, charset='utf8')
     return conn
 
